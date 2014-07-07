b80aacf23cae1c9fb6af2df144092ce47dbf8363
Jamie van Walsum <jvw@westnet.com.au>
2014-07-07 10:15:02 +0800

    minor refactoring

--- SchedulingImportRuns/MainWindow.xaml.cs
+++ SchedulingImportRuns/MainWindow.xaml.cs
@@ -124,7 +124,6 @@ namespace SchedulingImportRuns
         }
         private void ExportData()
         {
-
             try
             {
                 CsvFileDescription outputFileDescription = new CsvFileDescription { FirstLineHasColumnNames = true };
